788ae2883047694b086d0fa00a7fe3ff0c902544
fugr <fugr2012@gmail.com>
2016-03-31 11:24:00 +0800

    cluster/swarm:add cron Job register & remove

--- cluster/swarm/region.go
+++ cluster/swarm/region.go
@@ -1,8 +1,11 @@
 package swarm
 
 import (
+	"fmt"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/swarm/cluster"
+	"github.com/docker/swarm/cluster/swarm/database"
 	"github.com/docker/swarm/cluster/swarm/store"
 	"github.com/docker/swarm/utils"
 	crontab "gopkg.in/robfig/cron.v2"
@@ -12,7 +15,8 @@ type Region struct {
 	*Cluster
 
 	// addition by fugr
-	cron *crontab.Cron // crontab tasks
+	cron     *crontab.Cron // crontab tasks
+	cronJobs map[crontab.EntryID]*database.BackupStrategy
 
 	datacenters []*Datacenter
 	networkings []*Networking
@@ -34,6 +38,7 @@ func NewRegion(cli cluster.Cluster) (*Region, error) {
 	region := &Region{
 		Cluster:            cluster,
 		cron:               crontab.New(),
+		cronJobs:           make(map[crontab.EntryID]*database.BackupStrategy),
 		datacenters:        make([]*Datacenter, 0, 50),
 		networkings:        make([]*Networking, 0, 50),
 		services:           make([]*Service, 0, 100),
@@ -58,6 +63,30 @@ func (r *Region) generateUUID(length int) string {
 	}
 }
 
-func (r *Region) RegisterBackupStrategy(svc *Service) error {
+func (r *Region) RegisterBackupStrategy(id crontab.EntryID, strategy *database.BackupStrategy) error {
+	entry := r.cron.Entry(id)
+	if entry.ID != id {
+		return fmt.Errorf("Cron Job Not Found,%d", id)
+	}
+
+	r.Lock()
+	r.cronJobs[id] = strategy
+	r.Unlock()
+
+	return nil
+}
+
+func (r *Region) RemoveCronJob(strategyID string) error {
+	r.Lock()
+
+	for key, val := range r.cronJobs {
+		if val.ID == strategyID {
+			r.cron.Remove(key)
+			return nil
+		}
+	}
+
+	r.Unlock()
+
 	return nil
 }
