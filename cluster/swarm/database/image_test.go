450a81a7f14769d0a81645e22f2b220fe060ffa9
fugr <fugr2012@gmail.com>
2016-04-19 19:16:55 +0800

    cluster/swarm:add test

---
+++ cluster/swarm/database/image_test.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTXInsertUnitConfig(t *testing.T) {
+	db, err := GetDB(true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tx, err := db.Beginx()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer tx.Rollback()
+
+	config := &UnitConfig{
+		ID:            "test1",
+		ImageID:       "image1",
+		Path:          "/root/abc",
+		Version:       0,
+		ParentID:      "",
+		Content:       "",
+		ConfigKeySets: "",
+		KeySets:       map[string]bool{"abc": false, "def": true},
+		CreateAt:      time.Now(),
+	}
+
+	err = TXInsertUnitConfig(tx, config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
