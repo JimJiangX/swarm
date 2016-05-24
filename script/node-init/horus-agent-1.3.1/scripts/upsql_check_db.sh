987ebbb899106815d8fb5b4117f33f460a5e168d
linleybao <johnny.beaus@gmail.com>
2016-05-24 15:13:05 +0800

    node-init.sh/update version

---
+++ script/node-init/horus-agent-1.3.1/scripts/upsql_check_db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+container_name=$1
+docker inspect $container_name > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	status=critical
+fi
+
+docker exec $container_name /root/check_db  > /dev/null 2>&1
+if [  $? -eq 0 ];then
+	status=passing
+else
+	status=critical
+fi 
+
+echo $status
+
+
