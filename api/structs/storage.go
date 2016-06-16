3b74a429205c048e0f7258233bbaabbe7023adf6
fugr <fugr2012@gmail.com>
2016-06-16 20:10:40 +0800

    api/structs:fix Request Body

--- api/structs/storage.go
+++ api/structs/storage.go
@@ -2,10 +2,10 @@ package structs
 
 type PostSANStoreRequest struct {
 	Vendor       string
-	Addr         string `json:",omitempty"`
+	Addr         string `json:"ip_addr,omitempty"`
 	Username     string `json:",omitempty"`
 	Password     string `json:",omitempty"`
-	Admin        string `json:",omitempty"`
+	Admin        string `json:"admin_unit,omitempty"`
 	LunStart     int    `json:"lun_start,omitempty"`
 	LunEnd       int    `json:"lun_end,omitempty"`
 	HostLunStart int    `json:"host_lun_start,omitempty"`
