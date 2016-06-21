01551c3ffbadea90b507bb12ff8fdc597fc2a3f1
fugr <fugr2012@gmail.com>
2016-06-21 11:51:17 +0800

    Revert "cluster/swarm/database:fix timeZone of connection"
    This reverts commit 2755023ad62addd8e64ef6a070a2b98e4af3e0d0.

--- cluster/swarm/database/db_test.go
+++ cluster/swarm/database/db_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func init() {
-	dbSource = "root:111111@tcp(127.0.0.1:3306)/DBaaS?parseTime=true&charset=utf8&loc=Asia%2FShanghai&sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'"
+	dbSource = "root:111111@tcp(127.0.0.1:3306)/DBaaS?parseTime=true&charset=utf8&loc=Asia%%2FShanghai&sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'"
 	driverName = "mysql"
 }
 
