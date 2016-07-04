2ce5f17cbaee9ba421772d69b1b6b16de84580d2
linleybao <johnny.beaus@gmail.com>
2016-07-04 17:37:43 +0800

    fix bug

--- script/node-init/horus-agent-1.3.1/scripts/upsql_check_ps.sh
+++ script/node-init/horus-agent-1.3.1/scripts/upsql_check_ps.sh
@@ -2,7 +2,7 @@
 
 container_name=$1
 count=`ps -ef | grep "/usr/local/mysql/bin/mysqld --defaults-file" | grep ${container_name} | wc -l`
-if [ $count -ne 1 ]; then
+if [ $count -eq 1 ]; then
 	status="passing"
 else
 	status="critical"
