c7f9e91d2d223ca598d10095cf7bff743abc6870
fugr <fugr2012@gmail.com>
2016-08-18 18:31:21 +0800

    all:fix errors found by go vet

--- cluster/swarm/register_test.go
+++ cluster/swarm/register_test.go
@@ -3,10 +3,10 @@ package swarm
 import "testing"
 
 func TestDeregisterToHorus(t *testing.T) {
-	addr := "192.168.2.123:8000"
+
 	body := []string{"1234567890", "0987654321"}
 
-	err := deregisterToHorus(addr, true, body...)
+	err := deregisterToHorus(true, body...)
 	if err != nil {
 		t.Error(err)
 	}
