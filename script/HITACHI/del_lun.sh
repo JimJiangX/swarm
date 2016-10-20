aad2653f992bd8125884eedf6c54fac68e3d9ec7
linleybao <johnny.beaus@gmail.com>
2016-10-20 17:45:23 +0800

    update HITACHI scripts

--- script/HITACHI/del_lun.sh
+++ script/HITACHI/del_lun.sh
@@ -5,6 +5,11 @@ set -o nounset
 admin_unit=$1
 lun_id=$2
 
+auluref -unit ${admin_unit} -g -lu ${lun_id}
+if [ $? -ne 0 ]; then
+	echo "The specified logical unit is not defined."
+	exit 0
+fi
 
 expect << EOF
 	set timeout 3
@@ -14,7 +19,21 @@ expect << EOF
 	"y/n" {send "y\r"; exp_continue}
 	"y/n" {send "y\r"}
 }
-expect eof
 EOF
 
-exit $?
+loop=0
+while(( $loop<=10 ))
+do
+	auluref -unit ${admin_unit} -g -lu ${lun_id}
+	if [ $? -ne 0 ]; then
+		echo "delete lun succeeded!"
+		exit 0
+	fi
+	
+	let "loop++"
+	sleep 2
+done
+
+# if timeout over exit 1
+echo "delete lun failed !"
+exit 1
