dbe5e0458cbc45ec27a99d4f7dd9cf93f2aebe7f
fugr <fugr2012@gmail.com>
2016-10-20 13:47:56 +0800

    clsuter/swarm/database:fix var name

--- cluster/swarm/database/db_test.go
+++ cluster/swarm/database/db_test.go
@@ -8,11 +8,11 @@ import (
 func init() {
 	dbSource = "root:111111@tcp(192.168.2.121:3306)/DBaaS_test?parseTime=true&charset=utf8&loc=Asia%2FShanghai&sql_mode='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'"
 	driverName = "mysql"
-	dbMaxOpenConns = 8
+	dbMaxIdleConns = 8
 }
 
 func TestConnect(t *testing.T) {
-	db, err := Connect(driverName, dbSource, dbMaxOpenConns)
+	db, err := Connect(driverName, dbSource, dbMaxIdleConns)
 	if db == nil || err != nil {
 		t.Fatal(err)
 	}
@@ -30,7 +30,7 @@ func TestMustConnect(t *testing.T) {
 		}
 	}()
 
-	db := MustConnect(driverName, dbSource, dbMaxOpenConns)
+	db := MustConnect(driverName, dbSource, dbMaxIdleConns)
 	if db == nil {
 		t.FailNow()
 	}
