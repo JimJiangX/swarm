98212754f8c1f78cf26e594c454f7fc7767d0e92
linleybao <johnny.beaus@gmail.com>
2016-10-28 00:16:47 +0800

    fix multipath -f err

--- script/node-init/dbaas_volume_plugin-1.7.11/scripts/sanDeviceBlock.sh
+++ script/node-init/dbaas_volume_plugin-1.7.11/scripts/sanDeviceBlock.sh
@@ -9,12 +9,24 @@ do
 	subdev_name=`lsscsi | grep ":${hostlun_id}]" | grep "${vendor}" | head -n 1 | awk '{print $6}'`
 	if [ "$subdev_name" != '' ]; then
 		dev_name=`multipath -l ${subdev_name} | grep ${vendor} | awk '{print $1}'`
-		multipath -f ${dev_name}
+		if [ "${dev_name}" != '' ]; then
+			multipath -f ${dev_name}
+			if [ $? -eq 0 ]; then
+				for name in `lsscsi | grep ":${hostlun_id}]" | grep "${vendor}" | awk '{print $1}' | sed  's/\[//g; s/\]//g'`
+				do
+					name=`echo ${name} | sed 's/\:/\ /g'`
+					echo "scsi remove-single-device ${name}" > /proc/scsi/scsi
+				done
+			else
+				echo "multipath flush device faild"
+				exit 1
+			fi
+		else
+			echo "cannot find multipath device"
+			exit 1
+		fi
+	else
+		echo "cannot find multipath subdevice"
+		exit 1
 	fi
-
-	for name in `lsscsi | grep ":${hostlun_id}]" | grep "${vendor}" | awk '{print $1}' | sed  's/\[//g; s/\]//g'`
-	do
-		name=`echo ${name} | sed 's/\:/\ /g'`
-		echo "scsi remove-single-device ${name}" > /proc/scsi/scsi
-	done
 done
