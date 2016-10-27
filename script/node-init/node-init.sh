b1699e43ada145aff2fa3b6a3297045111c14bb8
linleybao <johnny.beaus@gmail.com>
2016-10-27 09:42:40 +0800

    script:update local_volume_plugin:1.7.11

--- script/node-init/node-init.sh
+++ script/node-init/node-init.sh
@@ -475,7 +475,7 @@ init_docker() {
 
 # install docker plugin
 install_docker_plugin() {
-	local version=1.7.8
+	local version=1.7.11
 	local script_dir=/usr/local/local_volume_plugin/scripts
 	mkdir -p ${script_dir}
 
