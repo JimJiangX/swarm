ae6b0e0d7ce8cb73ff64074389485c784b6ddf50
linleybao <johnny.beaus@gmail.com>
2016-11-15 09:37:05 +0800

    vendor update smlib

--- vendor/github.com/yiduoyunQ/smlib/get.go
+++ vendor/github.com/yiduoyunQ/smlib/get.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/yiduoyunQ/sm/sm-svr/consts"
 	"github.com/yiduoyunQ/sm/sm-svr/structs"
 )
 
@@ -17,16 +16,12 @@ func GetProxy(ip string, port int, name string) (*structs.ProxyInfo, error) {
 		return nil, err
 	}
 	defer res.Body.Close()
-
 	b, _ := ioutil.ReadAll(res.Body)
-
 	if res.StatusCode != http.StatusOK {
 		return nil, errors.New(string(b))
 	}
-
 	var proxyInfo structs.ProxyInfo
 	json.Unmarshal(b, &proxyInfo)
-
 	return &proxyInfo, nil
 }
 
@@ -36,16 +31,12 @@ func GetProxys(ip string, port int) (map[string]*structs.ProxyInfo, error) {
 		return nil, err
 	}
 	defer res.Body.Close()
-
 	b, _ := ioutil.ReadAll(res.Body)
-
 	if res.StatusCode != http.StatusOK {
 		return nil, errors.New(string(b))
 	}
-
 	var proxyInfos map[string]*structs.ProxyInfo
 	json.Unmarshal(b, proxyInfos)
-
 	return proxyInfos, nil
 }
 
@@ -55,46 +46,28 @@ func GetTopology(ip string, port int) (*structs.Topology, error) {
 		return nil, err
 	}
 	defer res.Body.Close()
-
 	b, _ := ioutil.ReadAll(res.Body)
-
 	if res.StatusCode != http.StatusOK {
 		return nil, errors.New(string(b))
 	}
-
 	var topology structs.Topology
 	json.Unmarshal(b, &topology)
-
 	return &topology, nil
 }
 
-func GetServiceStatus(ip string, port int) (string, error) {
-	topology, err := GetTopology(ip, port)
-	if err != nil {
-		return "", err
-	}
-
-	serviceStatus := consts.StatusOK
-
-	for _, dbInfo := range topology.DataNodeGroup["default"] {
-		if dbInfo.Status != consts.Normal {
-			serviceStatus = consts.StatusWarning
-			if dbInfo.Type == consts.Master {
-				serviceStatus = consts.StatusError
-				break
-			}
-		}
-	}
-
-	return serviceStatus, nil
-}
-
 // need close res.Body in call function
+
 func get(ip string, port int, method, arg string) (*http.Response, error) {
-	uri := "http://" + ip + ":" + strconv.Itoa(port) + "/" + method
-	if arg != "" {
-		uri = uri + "/" + arg
+	var res *http.Response
+	var err error
+	if arg == "" {
+		res, err = http.Get("http://" + ip + ":" + strconv.Itoa(port) + "/" + method)
+	} else {
+		res, err = http.Get("http://" + ip + ":" + strconv.Itoa(port) + "/" + method + "/" + arg)
 	}
 
-	return http.Get(uri)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
--- vendor/github.com/yiduoyunQ/smlib/post.go
+++ vendor/github.com/yiduoyunQ/smlib/post.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"io"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -13,15 +13,15 @@ import (
 )
 
 func Ping(ip string, port int) error {
-	return post(nil, ip, port, "ping", "")
+	return post([]byte(""), ip, port, "ping", "")
 }
 
 func Lock(ip string, port int) error {
-	return post(nil, ip, port, "lock", "")
+	return post([]byte(""), ip, port, "lock", "")
 }
 
 func UnLock(ip string, port int) error {
-	return post(nil, ip, port, "unlock", "")
+	return post([]byte(""), ip, port, "unlock", "")
 }
 
 func SetProxys(ip string, port int, proxyNames []string) error {
@@ -29,7 +29,6 @@ func SetProxys(ip string, port int, proxyNames []string) error {
 	if err != nil {
 		return err
 	}
-
 	return post(b, ip, port, "proxys", "")
 }
 
@@ -42,24 +41,23 @@ func InitSm(ip string, port int, mgmPost structs.MgmPost) error {
 	if err != nil {
 		return err
 	}
-
 	return post(b, ip, port, "init", "")
 }
 
 func Isolate(ip string, port int, name string) error {
-	return post(nil, ip, port, "isolate", name)
+	return post([]byte(""), ip, port, "isolate", name)
 }
 
 func Recover(ip string, port int, name string) error {
-	return post(nil, ip, port, "recover", name)
+	return post([]byte(""), ip, port, "recover", name)
 }
 
 func AddUser(ip string, port int, user structs.User) error {
+	fmt.Printf("%+v\n", user)
 	b, err := json.Marshal(user)
 	if err != nil {
 		return err
 	}
-
 	return post(b, ip, port, "addUser", "")
 }
 
@@ -68,7 +66,6 @@ func UptUser(ip string, port int, user structs.User) error {
 	if err != nil {
 		return err
 	}
-
 	return post(b, ip, port, "uptUser", "")
 }
 
@@ -77,42 +74,33 @@ func DelUser(ip string, port int, user structs.User) error {
 	if err != nil {
 		return err
 	}
-
 	return post(b, ip, port, "delUser", "")
 }
 
 // need close res.Body in call function
-func post(body []byte, ip string, port int, method, arg string) error {
-	var (
-		reader   io.Reader = nil
-		bodyType           = "text/plain"
-		uri                = "http://" + ip + ":" + strconv.Itoa(port) + "/" + method
-	)
-
-	if arg != "" {
-		uri = uri + "/" + arg
+func post(b []byte, ip string, port int, method, arg string) error {
+	var res *http.Response
+	var err error
+	reader := bytes.NewReader(b)
+	if arg == "" {
+		res, err = http.Post("http://"+ip+":"+strconv.Itoa(port)+"/"+method, "POST", reader)
+	} else {
+		res, err = http.Post("http://"+ip+":"+strconv.Itoa(port)+"/"+method+"/"+arg, "POST", reader)
 	}
-	if body != nil {
-		reader = bytes.NewReader(body)
-		bodyType = "application/json"
-	}
-
-	res, err := http.Post(uri, bodyType, reader)
 	if err != nil {
 		return err
 	}
-	defer res.Body.Close()
 
+	defer res.Body.Close()
+	if err != nil {
+		return err
+	}
 	if res.StatusCode != http.StatusOK {
 		body, err := ioutil.ReadAll(res.Body)
 		if err != nil {
 			return err
 		}
-
 		return errors.New(string(body))
 	}
-        
-	io.CopyN(ioutil.Discard, res.Body, 1024)
-
 	return nil
 }
