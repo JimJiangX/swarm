e609bf6549ca3ee674a3ba65bc74dd87cd796607
fugr <fugr2012@gmail.com>
2016-11-23 13:07:14 +0800

    scplib:update test

--- scplib/scp_test.go
+++ scplib/scp_test.go
@@ -1,16 +1,35 @@
 package scplib
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
+
+const (
+	sshAddr  = "127.0.0.1:2222"
+	username = "vagrant"
+	password = "vagrant"
+)
 
 func newClient() (*Client, error) {
+	// vagrant up first
 
-	return NewClient("127.0.0.1", "fugr", "No.001")
+	return NewClient(sshAddr, username, password)
 }
 
-func TestNewClient(t *testing.T) {
+func TestExec(t *testing.T) {
 	c, err := newClient()
 	if err != nil {
-		t.Error(err, c != nil)
+		t.Fatal(err, c != nil)
+	}
+
+	out, err := c.Exec("/usr/bin/whoami")
+	if err != nil {
+		t.Error(err, string(out))
+	}
+
+	if got, want := string(bytes.TrimSpace(out)), username; got != want {
+		t.Errorf("Unexpected,want '%s' but got '%s'", want, got)
 	}
 
 	err = c.Close()
