e58ed8fd77fd0ba85ab742437e309674104b2c5e
linleybao <johnny.beaus@gmail.com>
2016-12-02 13:23:02 +0800

    script/node-init/dbaas_volume_plugin-1.7.16/scripts:fix bug

--- script/node-init/dbaas_volume_plugin-1.7.16/scripts/get_device_path.sh
+++ script/node-init/dbaas_volume_plugin-1.7.16/scripts/get_device_path.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 set -o nounset
 
+VENDOR=$1
+HLUN_ID=$2
 
-vendor=$1
-hostlun_id=$2
+check() {
+	n=`lsscsi -i *:*:*:${HLUN_ID} | awk '{print $7}' | uniq | wc -l`	
+	if [ ${n} -ne 1 ]; then
+		echo "Please set "user_friendly_names no" in /etc/multipath.conf"
+		exit 2
+	fi
+}
 
-#timeout 180*2=360(s)=6(min)
+get_mdevname() {
+	mdev_name=`lsscsi -i *:*:*:${HLUN_ID} | awk '{print $7}' | uniq`
+}
+
+#timeout 10*6=60(sec)=1(min)
 loop=0
-while(( $loop<=179 ))
+while(( $loop<=9 ))
 do
-	subdev_name=`lsscsi | grep ":${hostlun_id}]" | grep "${vendor}" | head -n 1 | awk '{print $6}'`
-	if [ "$subdev_name" != '' ]; then
-		dev_name=`multipath -l ${subdev_name} | grep ${vendor} | awk '{print $1}'`
-		if [ "${dev_name}" != '' ]; then 
-			echo /dev/mapper/$dev_name
-			exit 0
-		fi
-	fi	
+        sleep 6
+	check
+	get_mdevname
+	if [ "${mdev_name}" != '' ]; then 
+		echo /dev/mapper/${mdev_name}
+		exit 0
+	fi
         let "loop++"
-        sleep 2
 done
 
 echo "can't find device !"
 exit 1
+
