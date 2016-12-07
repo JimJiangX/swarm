fb8659741abb945042d23153c84cda21c4a783e4
fugr <fugr2012@gmail.com>
2016-12-07 16:39:11 +0800

    garden/resource:add kvstore to Datacenter

--- garden/resource/datacenter.go
+++ garden/resource/datacenter.go
@@ -3,17 +3,20 @@ package resource
 import (
 	"github.com/docker/swarm/cluster"
 	"github.com/docker/swarm/garden/database"
+	"github.com/docker/swarm/garden/kvstore"
 )
 
 type Datacenter struct {
-	clsuter cluster.Cluster
-	dco     database.ClusterOrmer
+	clsuter  cluster.Cluster
+	dco      database.ClusterOrmer
+	kvClient kvstore.Client
 }
 
-func NewDatacenter(dco database.ClusterOrmer, c cluster.Cluster) *Datacenter {
+func NewDatacenter(dco database.ClusterOrmer, c cluster.Cluster, client kvstore.Client) *Datacenter {
 	return &Datacenter{
-		dco:     dco,
-		clsuter: c,
+		dco:      dco,
+		clsuter:  c,
+		kvClient: client,
 	}
 }
 
