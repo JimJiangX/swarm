bce01bbcce571ca34f6a0db05f9ccb0b54c02bb4
fugr <fugr2012@gmail.com>
2016-12-16 17:08:41 +0800

    clsuter:update Cluster interface

--- cluster/mesos/addition.go
+++ cluster/mesos/addition.go
@@ -1,8 +1,6 @@
 package mesos
 
-import (
-	"github.com/docker/swarm/cluster"
-)
+import "github.com/docker/swarm/cluster"
 
 func (c *Cluster) EngineByAddr(addr string) *cluster.Engine {
 	return nil
@@ -11,3 +9,9 @@ func (c *Cluster) EngineByAddr(addr string) *cluster.Engine {
 func (c *Cluster) Engine(IDOrName string) *cluster.Engine {
 	return nil
 }
+
+func (c *Cluster) AddPendingContainer(name, swarmID, engineID string, config *cluster.ContainerConfig) error {
+	return nil
+}
+
+func (c *Cluster) RemovePendingContainer(swarmID string) {}
