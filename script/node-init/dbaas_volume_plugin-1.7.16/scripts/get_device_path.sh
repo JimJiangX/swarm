6d3e5e279dd96c820a2bd109b015c86587424500
linleybao <johnny.beaus@gmail.com>
2016-12-21 14:09:19 +0800

    script/node-init/dbaas_volume_plugin-1.7.16/scripts/get_device_path.sh: fix check multipath device name

--- script/node-init/dbaas_volume_plugin-1.7.16/scripts/get_device_path.sh
+++ script/node-init/dbaas_volume_plugin-1.7.16/scripts/get_device_path.sh
@@ -7,13 +7,9 @@ HLUN_ID=$2
 check() {
 	n=`lsscsi -i *:*:*:${HLUN_ID} | awk '{print $7}' | uniq | wc -l`	
 	if [ ${n} -ne 1 ]; then
-		echo "Please set "user_friendly_names no" in /etc/multipath.conf"
+		echo "Please start multipatchd.service and set \"user_friendly_names no\" in /etc/multipath.conf"
 		exit 2
 	fi
-}
-
-get_mdevname() {
-	mdev_name=`lsscsi -i *:*:*:${HLUN_ID} | awk '{print $7}' | uniq`
 }
 
 #timeout 10*6=60(sec)=1(min)
@@ -22,8 +18,10 @@ while(( $loop<=9 ))
 do
         sleep 6
 	check
-	get_mdevname
-	if [ "${mdev_name}" != '' ]; then 
+	mdev_name=`lsscsi -i *:*:*:${HLUN_ID} | awk '{print $7}' | uniq`
+	#wc -L print the length of the longest line
+	name_char_count=`cat ${mdev_name} | wc -L`
+	if [ "${mdev_name}" != '' && ${name_char_count} -eq 33 ]; then 
 		echo /dev/mapper/${mdev_name}
 		exit 0
 	fi
