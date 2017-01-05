c7f4b667731320b6e6da5982021d0362cd3504ba
linleybao <johnny.beaus@gmail.com>
2017-01-05 16:25:01 +0800

    fix cpu usage error

--- script/node-init/horus-agent-1.4.3/scripts/docker_stat.sh
+++ script/node-init/horus-agent-1.4.3/scripts/docker_stat.sh
@@ -8,9 +8,17 @@ if [ $# -lt 1 ];then
   	exit 2
 fi
 
+n=`docker inspect -f '{{.HostConfig.CpusetCpus}}' 19 | sed 's/,//g' | wc -m`
+cpu_num=$[n-1]
 data=`docker stats --no-stream ${container_name} 2>/dev/null| tail -n 1|awk '{print $2":"$8}' | tr -d % `
+cpu_usage=${data%%:*}
+#cpu=`echo "sclae=4;$cpu_usage/$cpu_num" | bc`
+cpu=`awk 'BEGIN{printf "%.2f",'$cpu_usage'/'$cpu_num'}'`
+mem=${data##*:}
+
+data=`echo $cpu":"$mem`
 #$2:CPU  % $8: MEM % 
-if [ "${data}" == "" ];then
+if [ "${cpu}" == "" ] || [ "${mem}" == "" ]; then
 	echo "get data fail"
 	exit 2
 fi
