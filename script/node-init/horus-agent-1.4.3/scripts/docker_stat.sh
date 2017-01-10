06e0c84242aeab5508037d78949a7a1e36d35e11
linleybao <johnny.beaus@gmail.com>
2017-01-10 09:36:42 +0800

    fix typo

--- script/node-init/horus-agent-1.4.3/scripts/docker_stat.sh
+++ script/node-init/horus-agent-1.4.3/scripts/docker_stat.sh
@@ -8,7 +8,7 @@ if [ $# -lt 1 ];then
   	exit 2
 fi
 
-n=`docker inspect -f '{{.HostConfig.CpusetCpus}}' 19 | sed 's/,//g' | wc -m`
+n=`docker inspect -f '{{.HostConfig.CpusetCpus}}' ${container_name} | sed 's/,//g' | wc -m`
 cpu_num=$[n-1]
 data=`docker stats --no-stream ${container_name} 2>/dev/null| tail -n 1|awk '{print $2":"$8}' | tr -d % `
 cpu_usage=${data%%:*}
