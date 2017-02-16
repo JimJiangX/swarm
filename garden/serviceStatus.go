7f68d8d91da0d74ddf661f728951962604fa42a8
fugr <fugr2012@gmail.com>
2017-02-16 14:27:15 +0800

    garden:update service status

--- garden/serviceStatus.go
+++ garden/serviceStatus.go
@@ -8,63 +8,65 @@ import (
 )
 
 const (
+	_                              = iota           // 0
+	statusServcieBuilding          = iota<<4 + _ing // 1
+	statusServiceScheduling                         // 2
+	statusServiceAllocating                         // 3
+	statusServiceContainerCreating                  // 4
+	statusServiceStarting                           // 5,start contaier and start service
+	statusServiceStoping                            // 6
+	statusServiceBackuping                          // 7
+	statusServiceRestoring                          // 8
+	statusServiceUsersUpdating                      // 9
+	statusServiceScaling                            // 10
+	statusServiceConfigUpdating                     // 11
+	statusServiceUnitMigrating                      // 12
+	statusServiceUnitRebuilding                     // 13
+	statusServiceDeleting                           // 14
+
 	_ing    = 0
 	_done   = 1
 	_failed = 2
 
-	statusServcieBuilding    = 1<<4 + _ing
 	statusServcieBuilt       = statusServcieBuilding + _done
 	statusServcieBuildFailed = statusServcieBuilding + _failed
 
-	statusServiceScheduling     = 2<<4 + _ing
 	statusServiceScheduled      = statusServiceScheduling + _done
 	statusServiceScheduleFailed = statusServiceScheduling + _failed
 
-	statusServiceAllocating     = 3<<4 + _ing
 	statusServiceAllocated      = statusServiceAllocating + _done
 	statusServiceAllocateFailed = statusServiceAllocating + _failed
 
-	statusServiceContainerCreating     = 4<<4 + _ing
 	statusServiceContainerCreated      = statusServiceContainerCreating + _done
 	statusServiceContainerCreateFailed = statusServiceContainerCreating + _failed
 
-	statusServiceStarting    = 5<<4 + _ing // start contaier and start service
 	statusServiceStarted     = statusServiceStarting + _done
 	statusServiceStartFailed = statusServiceStarting + _failed
 
-	statusServiceStoping    = 6<<4 + _ing
 	statusServiceStoped     = statusServiceStoping + _done
 	statusServiceStopFailed = statusServiceStoping + _failed
 
-	statusServiceBackuping    = 7<<4 + _ing
 	statusServiceBackupDone   = statusServiceBackuping + _done
 	statusServiceBackupFailed = statusServiceBackuping + _failed
 
-	statusServiceRestoring     = 8<<4 + _ing
 	statusServiceRestored      = statusServiceRestoring + _done
 	statusServiceRestoreFailed = statusServiceRestoring + _failed
 
-	statusServiceUsersUpdating     = 9<<4 + _ing
 	statusServiceUsersUpdated      = statusServiceUsersUpdating + _done
 	statusServiceUsersUpdateFailed = statusServiceUsersUpdating + _failed
 
-	statusServiceScaling     = 10<<4 + _ing
 	statusServiceScaled      = statusServiceScaling + _done
 	statusServiceScaleFailed = statusServiceScaling + _failed
 
-	statusServiceConfigUpdating     = 11<<4 + _ing
 	statusServiceConfigUpdated      = statusServiceConfigUpdating + _done
 	statusServiceConfigUpdateFailed = statusServiceConfigUpdating + _failed
 
-	statusServiceUnitMigrating     = 12<<4 + _ing
 	statusServiceUnitMigrated      = statusServiceUnitMigrating + _done
 	statusServiceUnitMigrateFailed = statusServiceUnitMigrating + _failed
 
-	statusServiceUnitRebuilding    = 13<<4 + _ing
 	statusServiceUnitRebuilt       = statusServiceUnitRebuilding + _done
 	statusServiceUnitRebuildFailed = statusServiceUnitRebuilding + _failed
 
-	statusServiceDeleting     = 14<<4 + _ing
 	statusServiceDeleteFailed = statusServiceDeleting + _failed
 )
 
