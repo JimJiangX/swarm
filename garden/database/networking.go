43dea80a7bbd58321f827153079e51be6dd2f573
fugr <fugr2012@gmail.com>
2017-02-22 15:28:55 +0800

    garden/database:refactor IP & networking

--- garden/database/networking.go
+++ garden/database/networking.go
@@ -7,42 +7,48 @@ import (
 	"github.com/pkg/errors"
 )
 
+// TODO: combine Networking and IP
+
 type NetworkingOrmer interface {
 	// IP
 	ListIPByNetworking(networkingID string) ([]IP, error)
 	ListIPByUnitID(unit string) ([]IP, error)
 	ListIPWithCondition(networking string, allocation bool, num int) ([]IP, error)
 
-	AllocIPByNetworking(id, unit string) (IP, error)
-
-	// Port
-	ImportPort(start, end int, filter ...int) (int, error)
-
-	ListAvailablePorts(num int) ([]Port, error)
-	ListPortsByUnit(nameOrID string) ([]Port, error)
-	ListPorts(start, end, limit int) ([]Port, error)
-
-	DelPort(port int, allocated bool) error
+	AllocIPByNetworking(networkingID, _type, unit string) (IP, error)
 
 	// Networking
-	InsertNetworking(Networking, []IP) error
-	SetNetworkingStatus(ID string, enable bool) error
-
-	GetNetworkingByID(ID string) (Networking, int, error)
-	ListNetworking() ([]Networking, error)
-	ListNetworkingByType(_type string) ([]Networking, error)
-
+	InsertNetworking([]IP) error
 	DelNetworking(ID string) error
 	IsNetwrokingUsed(networking string) (bool, error)
+	// Port
+	//	ImportPort(start, end int, filter ...int) (int, error)
+
+	//	ListAvailablePorts(num int) ([]Port, error)
+	//	ListPortsByUnit(nameOrID string) ([]Port, error)
+	//	ListPorts(start, end, limit int) ([]Port, error)
+
+	//	DelPort(port int, allocated bool) error
+
+	// Networking
+
+	//	SetNetworkingStatus(ID string, enable bool) error
+
+	//	GetNetworkingByID(ID string) (Networking, int, error)
+	//	ListNetworking() ([]Networking, error)
+	//	ListNetworkingByType(_type string) ([]Networking, error)
 }
 
 // IP is table structure, associate to Networking
 type IP struct {
+	Allocated    bool   `db:"allocated"`
 	IPAddr       uint32 `db:"ip_addr"`
 	Prefix       int    `db:"prefix"`
+	ID           string `db:"id"`
 	NetworkingID string `db:"networking_id"`
+	Type         string `db:"type"`
 	UnitID       string `db:"unit_id"`
-	Allocated    bool   `db:"allocated"`
+	Gateway      string `db:"gateway"`
 }
 
 func (db dbBase) ipTable() string {
@@ -53,7 +59,7 @@ func (db dbBase) ipTable() string {
 func (db dbBase) ListIPByNetworking(networkingID string) ([]IP, error) {
 	var (
 		list  []IP
-		query = "SELECT ip_addr,prefix,networking_id,unit_id,allocated FROM " + db.ipTable() + " WHERE networking_id=?"
+		query = "SELECT id,allocated,ip_addr,prefix,networking_id,type,unit_id,gateway FROM " + db.ipTable() + " WHERE networking_id=?"
 	)
 
 	err := db.Select(&list, query, networkingID)
@@ -69,9 +75,9 @@ func (db dbBase) listIPsByAllocated(allocation bool, num int) ([]IP, error) {
 	)
 
 	if num > 0 {
-		query = fmt.Sprintf("SELECT ip_addr,prefix,networking_id,unit_id,allocated from %s WHERE allocated=? LIMIT %d", db.ipTable(), num)
+		query = fmt.Sprintf("SELECT id,allocated,ip_addr,prefix,networking_id,type,unit_id,gateway from %s WHERE allocated=? LIMIT %d", db.ipTable(), num)
 	} else {
-		query = fmt.Sprintf("SELECT ip_addr,prefix,networking_id,unit_id,allocated from %s WHERE allocated=?", db.ipTable())
+		query = fmt.Sprintf("SELECT id,allocated,ip_addr,prefix,networking_id,type,unit_id,gateway from %s WHERE allocated=?", db.ipTable())
 	}
 
 	err := db.Select(&out, query, allocation)
@@ -83,7 +89,7 @@ func (db dbBase) listIPsByAllocated(allocation bool, num int) ([]IP, error) {
 func (db dbBase) ListIPByUnitID(unit string) ([]IP, error) {
 	var (
 		out   []IP
-		query = "SELECT ip_addr,prefix,networking_id,unit_id,allocated from " + db.ipTable() + " WHERE unit_id=?"
+		query = "SELECT id,allocated,ip_addr,prefix,networking_id,type,unit_id,gateway from " + db.ipTable() + " WHERE unit_id=?"
 	)
 
 	err := db.Select(&out, query, unit)
@@ -95,7 +101,7 @@ func (db dbBase) ListIPByUnitID(unit string) ([]IP, error) {
 func (db dbBase) ListIPWithCondition(networking string, allocation bool, num int) ([]IP, error) {
 	var (
 		out   []IP
-		query = fmt.Sprintf("SELECT ip_addr,prefix,networking_id,unit_id,allocated from %s WHERE networking_id=? AND allocated=? LIMIT %d", db.ipTable(), num)
+		query = fmt.Sprintf("SELECT id,allocated,ip_addr,prefix,networking_id,type,unit_id,gateway from %s WHERE networking_id=? AND allocated=? LIMIT %d", db.ipTable(), num)
 	)
 
 	err := db.Select(&out, query, networking, allocation)
@@ -104,25 +110,41 @@ func (db dbBase) ListIPWithCondition(networking string, allocation bool, num int
 }
 
 // TxAllocIPByNetworking update IP UnitID in Tx
-func (db dbBase) AllocIPByNetworking(id, unit string) (IP, error) {
+func (db dbBase) AllocIPByNetworking(networking, _type, unit string) (IP, error) {
 	out := &IP{}
 
 	do := func(tx *sqlx.Tx) error {
 
-		query := "SELECT ip_addr,prefix FROM " + db.ipTable() + " WHERE networking_id=? AND allocated=? LIMIT 1 FOR UPDATE;"
-		query = tx.Rebind(query)
+		if networking != "" {
 
-		err := tx.Get(out, query, id, false)
-		if err != nil {
-			return errors.Wrap(err, "Tx get available IP")
+			query := "SELECT id,ip_addr,prefix,gateway FROM " + db.ipTable() + " WHERE networking_id=? AND allocated=? LIMIT 1 FOR UPDATE;"
+			query = tx.Rebind(query)
+
+			err := tx.Get(out, query, networking, false)
+			if err != nil {
+				return errors.Wrap(err, "Tx get available IP")
+			}
+
+		} else if _type != "" {
+
+			query := "SELECT id,ip_addr,prefix,gateway FROM " + db.ipTable() + " WHERE type=? AND allocated=? LIMIT 1 FOR UPDATE;"
+			query = tx.Rebind(query)
+
+			err := tx.Get(out, query, _type, false)
+			if err != nil {
+				return errors.Wrap(err, "Tx get available IP")
+			}
+
+		} else {
+			return errors.New("neither networkingID nor type is null")
 		}
 
 		out.Allocated = true
 		out.UnitID = unit
 
-		query = "UPDATE " + db.ipTable() + " SET allocated=:allocated,unit_id=:unit_id WHERE ip_addr=:ip_addr AND prefix=:prefix"
+		query := "UPDATE " + db.ipTable() + " SET allocated=?,unit_id=? WHERE id=?"
 
-		_, err = tx.NamedExec(query, *out)
+		_, err := tx.Exec(query, true, unit, out.ID)
 
 		return errors.Wrap(err, "tx update IP")
 	}
@@ -155,249 +177,10 @@ func (db dbBase) txSetIPs(tx *sqlx.Tx, val []IP) error {
 	return errors.Wrap(err, "Tx update []IP")
 }
 
-// Port is table structure,correspod with computer network port that a network applications listens on
-type Port struct {
-	Port      int    `db:"port"`
-	Name      string `db:"name"`
-	UnitID    string `db:"unit_id" json:"-"`
-	UnitName  string `db:"unit_name"`
-	Proto     string `db:"proto"` // tcp/udp
-	Allocated bool   `db:"allocated" json:"-"`
-}
-
-func (db dbBase) portTable() string {
-	return db.prefix + "_port"
-}
-
-// ListAvailablePorts returns []Port with len==num and Allocated==false
-func (db dbBase) ListAvailablePorts(num int) ([]Port, error) {
-	if num == 0 {
-		return nil, nil
-	}
-
-	var (
-		ports []Port
-		query = fmt.Sprintf("SELECT port,name,unit_id,unit_name,proto,allocated FROM %s WHERE allocated=? LIMIT %d", db.portTable(), num)
-	)
-
-	err := db.Select(&ports, query, false)
-	if err != nil {
-		return nil, errors.Wrap(err, "list []Port")
-	}
-
-	if len(ports) != num {
-		return ports, errors.Errorf("unable to get required num=%d available ports", num)
-	}
-
-	return ports, nil
-}
-
-// txSetPorts update []Port Name\UnitID\UnitName\Proto\Allocated in Tx
-func (db dbBase) txSetPorts(tx *sqlx.Tx, ports []Port) error {
-
-	query := "UPDATE " + db.portTable() + " SET name=:name,unit_id=:unit_id,unit_name=:unit_name,proto=:proto,allocated=:allocated WHERE port=:port"
-
-	stmt, err := tx.PrepareNamed(query)
-	if err != nil {
-		return errors.Wrap(err, "Tx prepare update []Port")
-	}
-
-	for i := range ports {
-		_, err = stmt.Exec(&ports[i])
-		if err != nil {
-			stmt.Close()
-
-			return errors.Wrap(err, "Tx update Port")
-		}
-	}
-
-	err = stmt.Close()
-
-	return errors.Wrap(err, "tx update []Port")
-}
-
-// ImportPort import Port from [start:end],returns number of insert ports.
-func (db dbBase) ImportPort(start, end int, filter ...int) (int, error) {
-	ports := make([]Port, 0, end-start+1)
-
-loop:
-	for i := start; i <= end; i++ {
-		for _, val := range filter {
-			if i == val {
-				continue loop
-			}
-		}
-
-		ports = append(ports, Port{
-			Port:      i,
-			Allocated: false,
-		})
-	}
-
-	err := db.insertPorts(ports)
-	if err == nil {
-		return len(ports), nil
-	}
-
-	return 0, err
-}
-
-func (db dbBase) insertPorts(ports []Port) error {
+func (db dbBase) InsertNetworking(ips []IP) error {
 	do := func(tx *sqlx.Tx) error {
 
-		query := "INSERT INTO " + db.portTable() + "(port,name,unit_id,unit_name,proto,allocated) VALUES (:port,:name,:unit_id,:unit_name,:proto,:allocated)"
-
-		stmt, err := tx.PrepareNamed(query)
-		if err != nil {
-			return errors.Wrap(err, "Tx prepare insert Port")
-		}
-
-		for i := range ports {
-			_, err = stmt.Exec(&ports[i])
-			if err != nil {
-				stmt.Close()
-
-				return errors.Wrap(err, "Tx insert Port")
-			}
-		}
-
-		stmt.Close()
-
-		return errors.Wrap(err, "Tx insert []Port")
-	}
-
-	return db.txFrame(do)
-}
-
-// ListPortsByUnit returns []Port select by UnitID or UnitName
-func (db dbBase) ListPortsByUnit(nameOrID string) ([]Port, error) {
-	var (
-		ports []Port
-		query = "SELECT port,name,unit_id,unit_name,proto,allocated FROM " + db.portTable() + " WHERE unit_id=? OR unit_name=?"
-	)
-
-	err := db.Select(&ports, query, nameOrID, nameOrID)
-
-	return ports, errors.Wrap(err, "list []Port")
-}
-
-// ListPorts returns []Port by condition start\end\limit.
-func (db dbBase) ListPorts(start, end, limit int) ([]Port, error) {
-	var (
-		err   error
-		ports []Port
-	)
-
-	switch {
-	case start == 0 && end == 0:
-		query := fmt.Sprintf("SELECT port,name,unit_id,unit_name,proto,allocated FROM %s LIMIT %d", db.portTable(), limit)
-		err = db.Select(&ports, query)
-
-	case start > 0 && end > 0:
-		query := fmt.Sprintf("SELECT port,name,unit_id,unit_name,proto,allocated FROM %s WHERE port>=? AND port<=? LIMIT %d", db.portTable(), limit)
-		err = db.Select(&ports, query, start, end)
-
-	case end == 0:
-		query := fmt.Sprintf("SELECT port,name,unit_id,unit_name,proto,allocated FROM %s WHERE port>=? LIMIT %d", db.portTable(), limit)
-		err = db.Select(&ports, query, start)
-
-	case start == 0:
-		query := fmt.Sprintf("SELECT port,name,unit_id,unit_name,proto,allocated FROM %s WHERE port<=? LIMIT %d", db.portTable(), limit)
-		err = db.Select(&ports, query, end)
-
-	default:
-		return nil, errors.Errorf("illegal input,start=%d end=%d limit=%d", start, end, limit)
-	}
-
-	return ports, errors.Wrap(err, "list []Port")
-}
-
-// DelPort delete by port,only if Port.Allocated==allocated
-func (db dbBase) DelPort(port int, allocated bool) error {
-	using := false
-
-	err := db.Get(&using, "SELECT allocated FROM "+db.portTable()+" WHERE port=?", port)
-	if err != nil {
-		return errors.Wrap(err, "get Port.Allocated")
-	}
-
-	if using != allocated {
-		return errors.Errorf("Port %d (%t!= %t),cannot be removed", port, using, allocated)
-	}
-
-	_, err = db.Exec("DELETE FROM "+db.portTable()+" WHERE port=? AND allocated=?", port, allocated)
-
-	return errors.Wrap(err, "delete Port")
-}
-
-// Networking is table structure,a goroup of IP Address
-type Networking struct {
-	ID      string `db:"id"`
-	Type    string `db:"type"`
-	Gateway string `db:"gateway"`
-	Enabled bool   `db:"enabled"`
-}
-
-func (db dbBase) networkingTable() string {
-	return db.prefix + "_networking"
-}
-
-// GetNetworkingByID returns Networking and IP.Prefix select by Networking ID.
-func (db dbBase) GetNetworkingByID(ID string) (Networking, int, error) {
-	net := Networking{}
-
-	err := db.Get(&net, "SELECT id,type,gateway,enabled FROM "+db.networkingTable()+" WHERE id=?", ID)
-	if err != nil {
-		return net, 0, errors.Wrap(err, "get Networking by ID:"+ID)
-	}
-
-	prefix := 0
-	err = db.Get(&prefix, "SELECT prefix FROM "+db.ipTable()+" WHERE networking_id=? LIMIT 1", ID)
-	if err != nil {
-		return net, 0, errors.Wrap(err, "get IP.Prefix")
-	}
-
-	return net, prefix, nil
-}
-
-// ListNetworkingByType returns []Netwroking select by Networking.Type
-func (db dbBase) ListNetworkingByType(_type string) ([]Networking, error) {
-	var (
-		list  []Networking
-		query = "SELECT id,type,gateway,enabled FROM " + db.networkingTable() + " WHERE type=?"
-	)
-
-	err := db.Select(&list, query, _type)
-
-	return list, errors.Wrap(err, "list []Networking by Type")
-}
-
-// ListNetworking returns all []Networking.
-func (db dbBase) ListNetworking() ([]Networking, error) {
-	var (
-		list  []Networking
-		query = "SELECT id,type,gateway,enabled FROM " + db.networkingTable() + ""
-	)
-
-	err := db.Select(&list, query)
-
-	return list, errors.Wrap(err, "list all []Networking")
-}
-
-// SetNetworkingStatus upate Networking Enabled by ID
-func (db dbBase) SetNetworkingStatus(ID string, enable bool) error {
-
-	query := "UPDATE " + db.networkingTable() + " SET enabled=? WHERE id=?"
-
-	_, err := db.Exec(query, enable, ID)
-
-	return errors.Wrap(err, "update Networking.Enabled")
-}
-
-func (db dbBase) InsertNetworking(net Networking, ips []IP) error {
-	do := func(tx *sqlx.Tx) error {
-
-		query := "INSERT INTO " + db.ipTable() + " (ip_addr,prefix,networking_id,unit_id,allocated) VALUES (:ip_addr,:prefix,:networking_id,:unit_id,:allocated)"
+		query := "INSERT INTO " + db.ipTable() + " (id,allocated,ip_addr,prefix,networking_id,type,unit_id,gateway) VALUES (:id,:allocated,:ip_addr,:prefix,:networking_id,:type,:unit_id,:gateway)"
 
 		stmt, err := tx.PrepareNamed(query)
 		if err != nil {
@@ -415,10 +198,6 @@ func (db dbBase) InsertNetworking(net Networking, ips []IP) error {
 
 		stmt.Close()
 
-		query = "INSERT INTO " + db.networkingTable() + " (id,type,gateway,enabled) VALUES (:id,:type,:gateway,:enabled)"
-
-		_, err = tx.NamedExec(query, &net)
-
 		return errors.Wrap(err, "Tx insert Networking")
 	}
 
@@ -426,15 +205,10 @@ func (db dbBase) InsertNetworking(net Networking, ips []IP) error {
 }
 
 // DelNetworking delete Networking and []IP in Tx
-func (db dbBase) DelNetworking(ID string) error {
+func (db dbBase) DelNetworking(networking string) error {
 	do := func(tx *sqlx.Tx) error {
 
-		_, err := tx.Exec("DELETE FROM "+db.networkingTable()+" WHERE id=?", ID)
-		if err != nil {
-			return errors.Wrap(err, "Tx delete Networking by ID")
-		}
-
-		_, err = tx.Exec("DELETE FROM "+db.ipTable()+" WHERE networking_id=?", ID)
+		_, err := tx.Exec("DELETE FROM "+db.ipTable()+" WHERE networking_id=?", networking)
 
 		return errors.Wrap(err, "Tx delete []IP by NetworkingID")
 	}
@@ -444,26 +218,15 @@ func (db dbBase) DelNetworking(ID string) error {
 
 // IsNetwrokingUsed returns true on below conditions:
 // one more IP belongs to networking has allocated
-// networking has used in Cluster
 func (db dbBase) IsNetwrokingUsed(networking string) (bool, error) {
 	var (
-		count        = 0
-		queryIP      = "SELECT COUNT(ip_addr) from " + db.ipTable() + " WHERE networking_id=? AND allocated=?"
-		queryCluster = "SELECT COUNT(id) from " + db.clusterTable() + " WHERE networking_id=?"
+		count = 0
+		query = "SELECT COUNT(id) from " + db.ipTable() + " WHERE networking_id=? AND allocated=?"
 	)
 
-	err := db.Get(&count, queryIP, networking, true)
+	err := db.Get(&count, query, networking, true)
 	if err != nil {
 		return false, errors.Wrap(err, "count []IP by NetworkingID")
-	}
-
-	if count > 0 {
-		return true, nil
-	}
-
-	err = db.Get(&count, queryCluster, networking)
-	if err != nil {
-		return false, errors.Wrap(err, "count []Cluster by NetworkingID")
 	}
 
 	return count > 0, nil
--- garden/resource/network.go
+++ garden/resource/network.go
@@ -6,52 +6,47 @@ import (
 	"github.com/pkg/errors"
 )
 
-type networks struct {
+type Networking struct {
 	nwo database.NetworkingOrmer
 }
 
-func newNetworks(nwo database.NetworkingOrmer) networks {
-	return networks{
+func NewNetworks(nwo database.NetworkingOrmer) Networking {
+	return Networking{
 		nwo: nwo,
 	}
 }
 
-func (nws networks) AddNetworking(start, end, gateway, _type string, prefix int) (database.Networking, []database.IP, error) {
+func (nw Networking) AddNetworking(start, end, gateway, networkingID, _type string, prefix int) (int, error) {
 	startU32 := utils.IPToUint32(start)
 	endU32 := utils.IPToUint32(end)
-	none := database.Networking{}
 
 	if move := uint(32 - prefix); (startU32 >> move) != (endU32 >> move) {
-		return none, nil, errors.Errorf("%s-%s is different network segments", start, end)
+		return 0, errors.Errorf("%s-%s is different network segments", start, end)
 	}
 	if startU32 > endU32 {
 		startU32, endU32 = endU32, startU32
-	}
-
-	net := database.Networking{
-		ID:      utils.Generate64UUID(),
-		Type:    _type,
-		Gateway: gateway,
-		Enabled: true,
 	}
 
 	num := int(endU32 - startU32 + 1)
 	ips := make([]database.IP, num)
 	for i := range ips {
 		ips[i] = database.IP{
+			ID:           utils.Generate32UUID(),
 			IPAddr:       startU32,
 			Prefix:       prefix,
-			NetworkingID: net.ID,
+			NetworkingID: networkingID,
+			Type:         _type,
+			Gateway:      gateway,
 			Allocated:    false,
 		}
 
 		startU32++
 	}
 
-	err := nws.nwo.InsertNetworking(net, ips)
+	err := nw.nwo.InsertNetworking(ips)
 	if err != nil {
-		return none, nil, err
+		return 0, err
 	}
 
-	return net, ips, nil
+	return len(ips), nil
 }
