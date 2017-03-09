51b2a4b9abd96c8ee38ea6796834a4e828b60809
linleybao <johnny.beaus@gmail.com>
2017-03-09 16:42:43 +0800

    add loop in check

--- script/node-init/dbaas_volume_plugin-1.7.16/scripts/get_device_path.sh
+++ script/node-init/dbaas_volume_plugin-1.7.16/scripts/get_device_path.sh
@@ -5,16 +5,23 @@ VENDOR=$1
 HLUN_ID=$2
 
 check() {
+#timeout 3*2=6(sec)
+loop=0
+while(( $loop<=3 ))
+do
 	n=`lsscsi -i *:*:*:${HLUN_ID} | awk '{print $NF}' | uniq | wc -l`	
 	if [ ${n} -ne 1 ]; then
 		echo "Please start multipatchd.service and set \"user_friendly_names no\" in /etc/multipath.conf"
 		exit 2
 	fi
+        let "loop++"
+        sleep 2
+done
 }
 
-#timeout 20*3=60(sec)=1(min)
+#timeout 18*3=54(sec)
 loop=0
-while(( $loop<=19 ))
+while(( $loop<=17 ))
 do
 	check
 	mdev_name=`lsscsi -i *:*:*:${HLUN_ID} | awk '{print $NF}' | uniq`
