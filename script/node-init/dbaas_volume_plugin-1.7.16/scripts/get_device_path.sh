f20060cba9bcd689237e8f0a468eab822769795d
linleybao <johnny.beaus@gmail.com>
2017-03-09 18:50:47 +0800

    optimized logic

--- script/node-init/dbaas_volume_plugin-1.7.16/scripts/get_device_path.sh
+++ script/node-init/dbaas_volume_plugin-1.7.16/scripts/get_device_path.sh
@@ -4,26 +4,14 @@ set -o nounset
 VENDOR=$1
 HLUN_ID=$2
 
-check() {
-#timeout 3*2=6(sec)
+#timeout 20*3=60(sec)
 loop=0
-while(( $loop<=3 ))
+while(( $loop<=19 ))
 do
 	n=`lsscsi -i *:*:*:${HLUN_ID} | awk '{print $NF}' | uniq | wc -l`	
 	if [ ${n} -ne 1 ]; then
-		echo "Please start multipatchd.service and set \"user_friendly_names no\" in /etc/multipath.conf"
-		exit 2
+		continue
 	fi
-        let "loop++"
-        sleep 2
-done
-}
-
-#timeout 18*3=54(sec)
-loop=0
-while(( $loop<=17 ))
-do
-	check
 	mdev_name=`lsscsi -i *:*:*:${HLUN_ID} | awk '{print $NF}' | uniq`
 	name_char_count=`echo -n ${mdev_name} | wc -m`
 	if [ "${mdev_name}" != '' ] && [ ${name_char_count} -eq 33 ]; then 
