a32cf7c75b4542646411ddced31ae8820f5848fb
fugr <fugr2012@gmail.com>
2017-04-11 10:34:27 +0800

    garden/resource:add TestAlloctCPUMemory

--- garden/resource/alloc_test.go
+++ garden/resource/alloc_test.go
@@ -1,6 +1,12 @@
 package resource
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/swarm/cluster"
+	"github.com/docker/swarm/scheduler/node"
+)
 
 func TestFindIdleCPUs(t *testing.T) {
 	want := "2,4,6,7"
@@ -21,9 +27,59 @@ func TestFindIdleCPUs(t *testing.T) {
 		t.Errorf("Unexpected,want '%s' but got '%s'", want, got)
 	}
 
+	want = "0,10,11,12"
+	got, err = findIdleCPUs([]string{"1-9", "13"}, 14, 4)
+	if err != nil {
+		t.Error(err)
+	}
+	if got != want {
+		t.Errorf("Unexpected,want '%s' but got '%s'", want, got)
+	}
+
 	got, err = findIdleCPUs([]string{"0,1", "5,8", "3", "7,9"}, 11, 5)
 	if err == nil {
 		t.Errorf("error expected,but got '%s'", got)
 	}
-	t.Log(got, err)
+}
+
+func TestAlloctCPUMemory(t *testing.T) {
+	config := cluster.ContainerConfig{}
+	containers := []*cluster.Container{
+		&cluster.Container{
+			Config: &cluster.ContainerConfig{
+				HostConfig: container.HostConfig{
+					Resources: container.Resources{
+						CpusetCpus: "1-9,15",
+						Memory:     1 << 22,
+					}}}},
+	}
+	nd := node.Node{
+		Containers:  cluster.Containers(containers),
+		UsedMemory:  8 << 30,
+		UsedCpus:    10,
+		TotalMemory: 1 << 35,
+		TotalCpus:   16,
+	}
+
+	actor := NewAllocator(nil, nil)
+	sets, err := actor.AlloctCPUMemory(&config, &nd, 5, 1<<34, nil)
+	if err != nil {
+		t.Error(err, sets)
+	}
+
+	if err == nil && (sets != "0,10,11,12,13" ||
+		sets != config.HostConfig.CpusetCpus ||
+		config.HostConfig.Memory != 1<<34) {
+		t.Error(sets, config.HostConfig.CpusetCpus, config.HostConfig.Memory)
+	}
+
+	sets, err = actor.AlloctCPUMemory(&config, &nd, 3, 1<<34, []string{"0-13"})
+	if err == nil {
+		t.Errorf("error expected but got '%s'", sets)
+	}
+
+	sets, err = actor.AlloctCPUMemory(&config, &nd, 3, 1<<35-8<<30+1, []string{"0-11"})
+	if err == nil {
+		t.Errorf("expected Memory is unavailable, but got '%s'", sets)
+	}
 }
