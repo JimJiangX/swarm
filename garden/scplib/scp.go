b674a787340580e36e09a007e14af950b205e4bb
fugr <fugr2012@gmail.com>
2017-05-18 16:44:07 +0800

    scplib:add NewClientByPublicKeys

--- garden/scplib/scp.go
+++ garden/scplib/scp.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"net"
 	"os"
+	"path/filepath"
 
 	"github.com/hnakamur/go-scp"
 	"github.com/pkg/errors"
@@ -52,6 +53,53 @@ func NewScpClient(addr, user, password string) (ScpClient, error) {
 	}
 
 	return &client{c}, nil
+}
+
+// NewClientByPublicKeys returns a pointer of Client,ssh client with authenticate.
+// rsa default "$HOME/.ssh/id_rsa"
+func NewClientByPublicKeys(addr, user, rsa string) (*Client, error) {
+	if rsa == "" {
+		home := os.Getenv("HOME")
+		rsa = filepath.Join(home, "/.ssh/id_rsa")
+	}
+
+	var hostKey ssh.PublicKey
+	// A public key may be used to authenticate against the remote
+	// server by using an unencrypted PEM-encoded private key file.
+	//
+	// If you have an encrypted private key, the crypto/x509 package
+	// can be used to decrypt it.
+	key, err := ioutil.ReadFile(rsa)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to read private key")
+	}
+
+	// Create the Signer for this private key.
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to parse private key")
+	}
+
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			// Use the PublicKeys method for remote authentication.
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.FixedHostKey(hostKey),
+	}
+
+	// Connect to the remote server and perform the SSH handshake.
+	client, err := ssh.Dial("tcp", addr, config)
+	if err != nil {
+		if client != nil {
+			client.Close()
+		}
+
+		return nil, errors.Wrap(err, "unable to connect")
+	}
+
+	return &Client{client}, nil
 }
 
 // An implementation of ssh.KeyboardInteractiveChallenge that simply sends
