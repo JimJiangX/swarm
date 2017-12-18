08e92bf846b4af0069aefa8fd33a120920c873b2
linleybao <johnny.beaus@gmail.com>
2017-12-18 08:48:32 +0800

    add set sysctl

--- script/node-init/node-init.sh
+++ script/node-init/node-init.sh
@@ -94,6 +94,39 @@ if [ ${container_nic} = '' ]; then
 	exit 2
 fi
 
+set_sysctl() {
+	cat << EOF > /etc/sysctl.conf
+net.ipv4.ip_forward = 0
+net.ipv6.conf.all.forwarding = 0
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_tw_reuse = 1
+vm.dirty_ratio = 1
+vm.dirty_background_ratio = 1
+vm.dirty_writeback_centisecs = 10
+vm.dirty_expire_centisecs = 3
+vm.drop_caches = 1
+vm.swappiness = 0
+vm.vfs_cache_pressure = 200
+vm.pagecache_limit_mb = 10240
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+net.ipv4.conf.default.accept_source_route = 0
+net.ipv4.conf.default.rp_filter = 1
+net.ipv4.conf.default.secure_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.icmp_ignore_bogus_error_responses = 1
+net.ipv4.tcp_max_syn_backlog = 4096
+net.ipv4.tcp_syncookies = 1
+fs.aio-max-nr = 262144
+EOF
+	sysctl -p
+}
+
 rpm_install() {
 	if [ "${release}" == "SUSE LINUX" ]; then
 		zypper --no-gpg-checks --non-interactive install nfs-utils curl sysstat mariadb-client ${PT}
@@ -532,6 +565,7 @@ EOF
 	fi
 }
 
+set_sysctl
 rpm_install
 nfs_mount
 init_hdd_vg
