cc3e4a6d1c501ca946137f13fd520c9253310077
Hubery <fugr2012@gmail.com>
2018-01-18 09:09:02 +0800

    plugin/parser:support more upproxy versions

--- plugin/parser/proxy.go
+++ plugin/parser/proxy.go
@@ -23,6 +23,9 @@ func init() {
 	register("upproxy", "1.1", &upproxyConfigV100{})
 	register("upproxy", "1.2", &upproxyConfigV100{})
 	register("upproxy", "1.3", &upproxyConfigV100{})
+
+	register("upproxy", "2.0", &upproxyConfigV100{})
+	register("upproxy", "2.1", &upproxyConfigV100{})
 }
 
 type proxyConfig struct {
