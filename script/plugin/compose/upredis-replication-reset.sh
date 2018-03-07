d3309e4ff0005be3bacf3b17dceee3618895cf68
linleybao <johnny.beaus@gmail.com>
2018-03-07 14:55:32 +0800

    update password & fix bug

--- script/plugin/compose/upredis-replication-reset.sh
+++ script/plugin/compose/upredis-replication-reset.sh
@@ -8,7 +8,7 @@ fi
 
 #nodelist=192.168.2.100:6375,192.168.2.101:6375,192.168.2.102:6375
 node_list=$1
-default_pass=dbaas
+default_pass=5aiup_rd
 
 MODE=sharding_replication
 CUR_DIR=`dirname $0`
@@ -24,9 +24,15 @@ node_arr=(${node_list//,/ })
 for node in ${node_arr[@]}
 do
     ip=${node%%:*}
-    port=${node##*:}
-    ${TOOLS_DIR}/redis-cli -h ${ip} -p ${port} -a ${default_pass} flushall
+    # check network
+    ping -w 6 -c 3 ${ip}
     if [ $? -ne 0 ]; then
+        echo "ping ${ip} failed"
+        exit 4
+    fi
+    port=${node##*:}
+    rel=$(${TOOLS_DIR}/redis-cli -h ${ip} -p ${port} -a ${default_pass} flushall | grep OK | wc -l)
+    if [ ${rel} -ne 1 ]; then
         echo "redis($node) flashall failed"
         exit 3
     fi
