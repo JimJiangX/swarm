5ed83a6234439d72cdc2ad9fbaff57867e6487dd
Hubery <fugr2012@gmail.com>
2018-08-06 15:33:24 +0800

    garden/database:add Done field into Task

--- garden/database/task.go
+++ garden/database/task.go
@@ -106,6 +106,7 @@ func NewTask(object, relate, linkto, desc string, label map[string]string, timeo
 // Task
 
 type Task struct {
+	Done bool ` db:"-" json:"done"`
 	task
 	errs  []error
 	label map[string]string
@@ -268,7 +269,7 @@ func (db dbBase) GetTask(ID string) (Task, error) {
 
 	err := db.Get(&tk, query, ID)
 
-	return Task{task: tk}, errors.Wrap(err, "get task by id:"+ID)
+	return Task{task: tk, Done: tk.Status > TaskRunningStatus}, errors.Wrap(err, "get task by id:"+ID)
 }
 
 func (db dbBase) ListTasks(link string, status int) ([]Task, error) {
@@ -298,7 +299,7 @@ func (db dbBase) ListTasks(link string, status int) ([]Task, error) {
 	if err == nil {
 		out := make([]Task, 0, len(tks))
 		for i := range tks {
-			out = append(out, Task{task: tks[i]})
+			out = append(out, Task{task: tks[i], Done: tks[i].Status > TaskRunningStatus})
 		}
 
 		return out, nil
@@ -322,7 +323,7 @@ func (db dbBase) txListTasks(tx *sqlx.Tx, status int) ([]Task, error) {
 	if err == nil {
 		out := make([]Task, 0, len(tks))
 		for i := range tks {
-			out = append(out, Task{task: tks[i]})
+			out = append(out, Task{task: tks[i], Done: tks[i].Status > TaskRunningStatus})
 		}
 
 		return out, nil
